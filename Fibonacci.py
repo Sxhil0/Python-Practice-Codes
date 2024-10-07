d44c9f52af446ff5a915a6445067df8d63777181
Sahil Ayare <97663625+Sxhil0@users.noreply.github.com>
2024-10-07 20:42:51 +0530

    Update Fibonacci.py

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,8 +1,9 @@
-num=int(input("Enter a Number:"))
-n1,n2=0,1
-print(n1,n2,end=" ")
-for i in range(2,num):
-    n3=n1+n2
-    n1=n2
-    n2=n3
-    print(n3,end=" ")
+def fibo(n):
+    series=[]
+    a,b=0,1
+    for i in range(n):
+        series.append(a)
+        a,b=b,a+b
+    return series
+n=int(input("Number:"))
+print("series",fibo(n))
