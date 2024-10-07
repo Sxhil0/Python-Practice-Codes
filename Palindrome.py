94972292081c114d4dcda9a54e325dd712340411
Sahil Ayare <97663625+Sxhil0@users.noreply.github.com>
2024-10-07 20:35:51 +0530

    Update Palindrome.py

--- Palindrome.py
+++ Palindrome.py
@@ -1,15 +1,4 @@
 def palindrome(value):
-    value_str=str(value)
-    start=0
-    end=len(value_str)-1
-    while start<end:
-        if value_str[start]!=value_str[end]:
-            return False  # Not a palindrome
-        start+=1
-        end-=1
-    return True  # Is a palindrome
-sn=input("Enter a string or number: ")
-if palindrome(sn):
-    print(sn,"is a palindrome")
-else:
-    print(sn,"is not a palindrome")
+    return value==value[::-1]
+value=input("Enter Something: ")
+print(palindrome(value))
